567226e8e1619d04119b6f6886fc0a37b8fec312
raineryue <jinlei_yue@sina.com>
2016-01-26 14:47:46 +0800

    Initial Commit

---
+++ 20160126004-UIKeyboard-RegisterTests/_0160126004_UIKeyboard_RegisterTests.m
@@ -0,0 +1,39 @@
+//
+//  _0160126004_UIKeyboard_RegisterTests.m
+//  20160126004-UIKeyboard-RegisterTests
+//
+//  Created by Rainer on 16/1/26.
+//  Copyright © 2016年 Rainer. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _0160126004_UIKeyboard_RegisterTests : XCTestCase
+
+@end
+
+@implementation _0160126004_UIKeyboard_RegisterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
